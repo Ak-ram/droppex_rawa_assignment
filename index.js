0939b78e736e91c6dc34e8b6bf78c157351a1749
Akram A. Abdelbasir <69124951+Ak-ram@users.noreply.github.com>
2024-03-20 19:10:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-toggleNavbarBackarrow.addEventListener('click',()=>navBar.classList.add('hide'))
-menuIcon.addEventListener('click',()=>navBar.classList.remove('hide'))
+// Toggle Sidebar
+toggleNavbarBackarrow.addEventListener("click", () => {navBar.classList.add("hide");menuIcon.classList.remove("hidden");});
+menuIcon.addEventListener("click", (e) => {navBar.classList.remove("hide");menuIcon.classList.add("hidden");});
