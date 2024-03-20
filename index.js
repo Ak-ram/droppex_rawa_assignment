dcfc397e347083bb62d8d97934d1bfcd718d6eb9
Restyled.io <commits@restyled.io>
2024-03-20 02:44:02 +0000

    Restyled by clang-format

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-toggleNavbarBackarrow.addEventListener('click',()=>navBar.classList.add('hide'))
-menuIcon.addEventListener('click',()=>navBar.classList.remove('hide'))
+toggleNavbarBackarrow.addEventListener('click',
+                                       () => navBar.classList.add('hide'))
+menuIcon.addEventListener('click', () => navBar.classList.remove('hide'))
